7751fb12d026c6a7174e15bef6111df501e08d6f
unknown <josemarianoreyesh02@gmail.com>
2023-03-31 19:25:30 -0600

    cambio de nombres

--- LabC.py
+++ LabC.py
@@ -1,29 +1,44 @@
+import re
+from tokens import Tokens
 from Regex_Postfix import convertExpression
 from Postfix_AFN import PostifixToAFN
-import re
 
+# variable del archivo
+archivo = 'YALex.txt'
 
-# Concatenamos los patrones en una única expresión regular
-regex = "0|1|2|3|4|5|6|7|8|9"
+# a los tokens del archivo los covertimos en arreglo
+tokenizer = Tokens()
+tokenizer.getTokens(archivo)
 
-# Creamos el automata a partir de la expresión regular
-conversion = convertExpression(len(regex))
+for i in range(len(tokenizer.tokens)):
+    for j in range(len(tokenizer.tokens)):
+        if i != j and tokenizer.tokens[i][0] in tokenizer.tokens[j][1]:
+            tokenizer.tokens[i] = (
+                tokenizer.tokens[i][0], tokenizer.tokens[i][1], True)
+            break
+        else:
+            tokenizer.tokens[i] = (
+                tokenizer.tokens[i][0], tokenizer.tokens[i][1], False)
 
-# llamada de funcion para convertir a postfix
-conversion.RegexToPostfix(regex)
+# por cada token en tokens creamos un afn
+for i, token in enumerate(tokenizer.tokens):
+    print('\n', i+1, token)
+    if token[2] == True:
+        try:
+            # Creamos el automata a partir de la expresión regular
+            conversion = convertExpression(len(token[1]))
 
-if conversion.ver == True:
-    postfix = conversion.res
-    print(regex)
-    print(postfix)
+            # llamada de funcion para convertir a postfix
+            conversion.RegexToPostfix(token[1])
+            if conversion.ver == True:
+                postfix = conversion.res
+                print(token[1])
+                print(postfix)
 
-    # instancia de clase para convertir a AFN
-    conversionAFN = PostifixToAFN(postfix)
+                # instancia de clase para convertir a AFN
+                conversionAFN = PostifixToAFN(postfix)
 
-    # llamada a metodo para convertir afn
-    conversionAFN.conversion()
-
-    # Ejemplo de uso
-    cadena = 'if (a > b) { print("a es mayor"); }'
-    tokens = re.findall(regex, cadena)
-    print(tokens)
+                # llamada a metodo para convertir afn
+                conversionAFN.conversion(i+1)
+        except:
+            print('no podemos generar ese afn aun')
